4a204607e6ee383c3927fa211430fbcb412691ca
Zhanibek Seidzhan <zhann2013@gmail.com>
2021-02-01 02:55:21 +0600

    html templates and routing
    html templates and routing.
    parsing url queries

--- test.js
+++ test.js
@@ -36,16 +36,69 @@ const url = require('url');
 
 // SERVER
 
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if(!product.organic) output = output.replace(/{%NOTORGANIC%}/g, "not-organic");
+
+    return output;
+}
+
+
+const tempOverview = fs.readFileSync(`${__dirname}/template/overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/template/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/template/product.html`, 'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer( (req, res) => {
+    
     
     const pathName  = req.url;
 
+    // Overview page
     if(pathName === '/overview' || pathName === '/') {
-        res.end('This is the Overview');
+
+        res.writeHead(200, {'Content-type': 'text/html'});
+
+        const cardsHtml = dataObj.map(el=>replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+
+        res.end(output);
+    
+    // Product page
     } else if (pathName === '/product') {
         res.end('This is the Product');
+
+    // API
+    } else if (pathName === '/api') {
+
+        // fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err,data)=> {
+        //     const productData = JSON.parse(data);
+        //     //console.log(productData);
+        //     res.writeHead(200, {'Content-type': 'application/json'});
+        //     res.end(data);
+        // });
+
+        res.writeHead(200, {'Content-type': 'application/json'});
+        res.end(data);
+
+    // Not found 404
+    } else {
+        res.writeHead(404, { 
+            'Content-type': 'text/html'
+        });
+        res.end('<h1>This page NOT FOUND</h1>');
     }
-    
+
 });
 
 server.listen(8000, '127.0.0.1', () => {
