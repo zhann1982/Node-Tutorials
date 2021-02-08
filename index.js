253b90a2f9dbf53000a27f30ee340bc49c8db747
Zhanibek Seidzhan <zhann2013@gmail.com>
2021-02-08 22:55:02 +0600

    finished simplest app - traversy
    finished simplest app - traversy tutorial.

--- index.js
+++ index.js
@@ -3,30 +3,83 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
-    if (req.url === '/') {
-        fs.readFile(path.join(__dirname,'public','index.html'),(err,content) => {
-            if (err) throw err;
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(content);
-        });
+    // if (req.url === '/') {
+    //     fs.readFile(path.join(__dirname,'public','index.html'),(err,content) => {
+    //         if (err) throw err;
+    //         res.writeHead(200, {'Content-Type': 'text/html'});
+    //         res.end(content);
+    //     });
+    // }
+
+    // if (req.url === '/about') {
+    //     fs.readFile(path.join(__dirname,'public','about.html'),(err,content) => {
+    //         if (err) throw err;
+    //         res.writeHead(200, {'Content-Type': 'text/html'});
+    //         res.end(content);
+    //     });
+    // }
+
+    // if (req.url === '/api/users') {
+    //     const users = [
+    //         {name: 'John', age: 23},
+    //         {name: 'Steve', age: 18}
+    //     ];
+    //     res.writeHead(200, {'Content-Type': 'application/json'});
+    //     res.end(JSON.stringify(users));
+    // }
+
+    // Build file path
+    let filePath = path.join(
+        __dirname,
+        'public',
+        req.url === '/'?'index.html':req.url
+    );
+
+    // Extension of file
+    let extname = path.extname(filePath);
+
+    // Initial content type
+    let contentType = 'text/html';
+
+    //Check ext and set content type
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript'
+            break;
+        case '.css':
+            contentType = 'text/css'
+            break;
+        case '.json':
+            contentType = 'application/json'
+            break;
+        case '.png':
+            contentType = 'image/png'
+            break;
+        case '.jpg':
+            contentType = 'image/jpg'
+            break;
     }
 
-    if (req.url === '/about') {
-        fs.readFile(path.join(__dirname,'public','about.html'),(err,content) => {
-            if (err) throw err;
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(content);
-        });
-    }
-
-    if (req.url === '/api/users') {
-        const users = [
-            {name: 'John', age: 23},
-            {name: 'Steve', age: 18}
-        ];
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify(users));
-    }
+    // Read file
+    fs.readFile(filePath, (err,content)=>{
+        if (err) {
+            if (err.code = 'EN0ENT') {
+                // Page not found
+                fs.readFile(path.join(__dirname,'public','404.html'), (err,content)=>{
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.end(content,'utf8');
+                });
+            } else {
+                // some server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`)
+            }
+        } else {
+            // Success
+            res.writeHead(200, {'Content-Type': contentType});
+            res.end(content,'utf8');
+        }
+    });
 });
 
 const PORT = process.env.PORT || 5000;
